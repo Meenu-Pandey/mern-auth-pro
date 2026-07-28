3c35f3dcce6e2ccdf8bed802edc2447ae5c488af
Meenu-pandey <pandeymeenu057@gmail.com>
2026-07-28 18:13:11 +0530

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
-app.use( cors({ origin: "http://localhost:5173", credentials: true, methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"], }) ); 
+app.use( cors({ origin: "https://mern-auth-pro.vercel.app", credentials: true, methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"], }) ); 
 app.options("*", cors());
 
 app.use(helmet());
